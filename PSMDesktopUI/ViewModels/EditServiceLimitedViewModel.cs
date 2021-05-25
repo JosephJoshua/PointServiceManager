84661c9c5cf97dfded80c3a86db279d4cd41012a
JosephJoshua <jj.anggita@gmail.com>
2021-05-26 01:08:51 +0900

    Add more editable service fields as a customer service

--- PSMDesktopUI/ViewModels/EditServiceLimitedViewModel.cs
+++ PSMDesktopUI/ViewModels/EditServiceLimitedViewModel.cs
@@ -11,11 +11,13 @@ namespace PSMDesktopUI.ViewModels
 {
     public class EditServiceLimitedViewModel : Screen
     {
-        private readonly IWindowManager _windowManager;
         private readonly IServiceEndpoint _serviceEndpoint;
 
         private ServiceModel _oldService;
         private int _nomorNota;
+
+        private string _kerusakan;
+        private string _noHp;
 
         private ServiceStatus _serviceStatuses;
         private ServiceStatus _selectedStatus;
@@ -23,6 +25,9 @@ namespace PSMDesktopUI.ViewModels
         private bool _sudahKonfirmasi;
         private string _isiKonfirmasi;
         private DateTime _tanggalKonfirmasi;
+
+        private double _dp;
+        private double _tambahanBiaya;
 
         public int NomorNota
         {
@@ -32,6 +37,28 @@ namespace PSMDesktopUI.ViewModels
             {
                 _nomorNota = value;
                 NotifyOfPropertyChange(() => NomorNota);
+            }
+        }
+
+        public string Kerusakan
+        {
+            get => _kerusakan;
+
+            set
+            {
+                _kerusakan = value;
+                NotifyOfPropertyChange(() => Kerusakan);
+            }
+        }
+
+        public string NoHp
+        {
+            get => _noHp;
+
+            set
+            {
+                _noHp = value;
+                NotifyOfPropertyChange(() => NoHp);
             }
         }
 
@@ -95,9 +122,30 @@ namespace PSMDesktopUI.ViewModels
             }
         }
 
-        public EditServiceLimitedViewModel(IWindowManager windowManager, IServiceEndpoint serviceEndpoint)
+        public double Dp
         {
-            _windowManager = windowManager;
+            get => _dp;
+
+            set
+            {
+                _dp = value;
+                NotifyOfPropertyChange(() => Dp);
+            }
+        }
+
+        public double TambahanBiaya
+        {
+            get => _tambahanBiaya;
+
+            set
+            {
+                _tambahanBiaya = value;
+                NotifyOfPropertyChange(() => TambahanBiaya);
+            }
+        }
+
+        public EditServiceLimitedViewModel(IServiceEndpoint serviceEndpoint)
+        {
             _serviceEndpoint = serviceEndpoint;
         }
 
@@ -105,10 +153,14 @@ namespace PSMDesktopUI.ViewModels
         {
             _oldService = service;
 
+            Kerusakan = service.Kerusakan;
+            NoHp = service.NoHp;
             NomorNota = service.NomorNota;
             SudahKonfirmasi = service.TanggalKonfirmasi.Year != 1753;
             IsiKonfirmasi = service.IsiKonfirmasi;
             TanggalKonfirmasi = service.TanggalKonfirmasi;
+            Dp = (double)service.Dp;
+            TambahanBiaya = (double)service.TambahanBiaya;
 
             SelectedStatus = Enum.GetValues(ServiceStatuses.GetType()).Cast<ServiceStatus>().Where((e) => e.Description() == service.StatusServisan).FirstOrDefault();
         }
@@ -134,14 +186,14 @@ namespace PSMDesktopUI.ViewModels
                 return false;
             }
 
-            if (tidakJadi && (_oldService.Biaya != 0 || _oldService.TambahanBiaya != 0))
+            if (tidakJadi && (_oldService.Biaya != 0 || TambahanBiaya != 0))
             {
                 if (DXMessageBox.Show(
                     "'Biaya' and 'Tambahan biaya' must be 0 if the service is cancelled. Do you want to set 'Biaya' and 'Tambahan biaya' to be 0?", "Edit service",
                     MessageBoxButton.YesNo) == MessageBoxResult.Yes)
                 {
                     _oldService.Biaya = 0;
-                    _oldService.TambahanBiaya = 0;
+                    TambahanBiaya = 0;
                 }
                 else
                 {
@@ -151,10 +203,14 @@ namespace PSMDesktopUI.ViewModels
 
             bool sudahDiambil = SelectedStatus == ServiceStatus.JadiSudahDiambil || SelectedStatus == ServiceStatus.TidakJadiSudahDiambil;
 
+            _oldService.Kerusakan = Kerusakan;
+            _oldService.NoHp = NoHp;
             _oldService.StatusServisan = SelectedStatus.Description();
             _oldService.IsiKonfirmasi = IsiKonfirmasi;
             _oldService.TanggalKonfirmasi = SudahKonfirmasi ? TanggalKonfirmasi : DateTime.MinValue;
             _oldService.TanggalPengambilan = sudahDiambil ? DateTime.Now : DateTime.MinValue;
+            _oldService.Dp = (decimal)Dp;
+            _oldService.TambahanBiaya = (decimal)TambahanBiaya;
 
             await _serviceEndpoint.Update(_oldService);
             return true;
